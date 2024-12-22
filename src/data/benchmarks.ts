b92ac0999c0c6d1658e6f6c0cbd71508a3c11fc8
Rasmus Lindahl <rasmus.lindahl1996@gmail.com>
2024-12-22 12:09:37 +0100

    Add rest of links for benchmarks

--- src/data/benchmarks.ts
+++ src/data/benchmarks.ts
@@ -11,39 +11,51 @@ export const benchmarkData: Benchmark[] = [
     benchmark: "ImageNet Challenge",
     release: "2009-01-01",
     solved: "2015-03-01",
-    url: "https://www.image-net.org/"
+    url: "https://www.image-net.org/",
+    paperUrl: "https://arxiv.org/pdf/1409.0575"
   },
   {
     benchmark: "WinoGrande",
     release: "2012-06-10",
     solved: "2023-07-11",
-    url: "https://winogrande.allenai.org/"
+    url: "https://winogrande.allenai.org/",
+    paperUrl: "https://arxiv.org/pdf/1907.10641"
   },
   {
     benchmark: "SQuAD 1.1",
     release: "2016-06-16",
     solved: "2017-06-15",
-    url: "https://rajpurkar.github.io/SQuAD-explorer/explore/1.1/dev/"
+    url: "https://rajpurkar.github.io/SQuAD-explorer/explore/1.1/dev/",
+    paperUrl: "https://arxiv.org/pdf/1606.05250"
   },
   {
     benchmark: "SQuAD 2.0",
     release: "2018-06-11",
     solved: "2018-06-15",
-    url: "https://rajpurkar.github.io/SQuAD-explorer/explore/v2.0/dev/"
+    url: "https://rajpurkar.github.io/SQuAD-explorer/explore/v2.0/dev/",
+    paperUrl: "https://arxiv.org/pdf/1806.03822"
   },
   {
     benchmark: "VQA",
     release: "2019-05-03",
     solved: "2021-06-15",
-    url: "https://visualqa.org/"
+    url: "https://visualqa.org/",
+    paperUrl: "https://arxiv.org/pdf/1505.00468"
   },
   {
     benchmark: "HellaSwag",
     release: "2019-06-19",
     solved: "2023-03-14",
-    url: "https://rowanzellers.com/hellaswag/"
+    url: "https://rowanzellers.com/hellaswag/",
+    paperUrl: "https://arxiv.org/pdf/1905.07830"
   },
-  { benchmark: "Adversarial NLI", release: "2019-10-31", solved: "2020-06-15", url: "https://github.com/facebookresearch/anli" },
+  { 
+    benchmark: "Adversarial NLI", 
+    release: "2019-10-31", 
+    solved: "2020-06-15", 
+    url: "https://github.com/facebookresearch/anli",
+    paperUrl: "https://arxiv.org/pdf/1910.14599"
+  },
   { benchmark: "ARC-AGI", release: "2019-11-05", solved: "2024-12-21", url: "https://arcprize.org/arc", paperUrl: "https://arxiv.org/abs/1911.01547" },
   { 
     benchmark: "SuperGLUE", 
