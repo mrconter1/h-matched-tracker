7d9f36f4115a06ec8bf3f202bfb8a552480431fd
Rasmus Lindahl <rasmus.lindahl1996@gmail.com>
2024-12-22 11:43:25 +0100

    Add more links

--- src/data/benchmarks.ts
+++ src/data/benchmarks.ts
@@ -52,9 +52,30 @@ export const benchmarkData: Benchmark[] = [
   },
   { benchmark: "Adversarial NLI", release: "2019-10-31", solved: "2020-06-15", timeToSolve: 0.62, url: "https://github.com/facebookresearch/anli" },
   { benchmark: "ARC-AGI", release: "2019-11-05", solved: "2024-12-21", timeToSolve: 5.13, url: "https://arcprize.org/arc", paperUrl: "https://arxiv.org/abs/1911.01547" },
-  { benchmark: "SuperGLUE", release: "2020-02-13", solved: "2020-03-15", timeToSolve: 0.08 },
-  { benchmark: "MMLU", release: "2020-09-07", solved: "2024-12-05", timeToSolve: 4.24 },
-  { benchmark: "MATH", release: "2021-11-08", solved: "2024-06-15", timeToSolve: 2.60 },
+  { 
+    benchmark: "SuperGLUE", 
+    release: "2020-02-13", 
+    solved: "2020-03-15", 
+    timeToSolve: 0.08,
+    url: "https://super.gluebenchmark.com/",
+    paperUrl: "https://arxiv.org/pdf/1905.00537"
+  },
+  { 
+    benchmark: "MMLU", 
+    release: "2020-09-07", 
+    solved: "2024-12-05", 
+    timeToSolve: 4.24,
+    url: "https://crfm.stanford.edu/helm/mmlu/latest/",
+    paperUrl: "https://arxiv.org/pdf/2009.03300"
+  },
+  { 
+    benchmark: "MATH", 
+    release: "2021-11-08", 
+    solved: "2024-06-15", 
+    timeToSolve: 2.60,
+    url: "https://github.com/hendrycks/math/",
+    paperUrl: "https://arxiv.org/pdf/2103.03874v2"
+  },
   { benchmark: "BIG-Bench-Hard", release: "2022-10-17", solved: "2024-06-21", timeToSolve: 1.68 },
   { benchmark: "GPQA", release: "2023-11-29", solved: "2024-09-21", timeToSolve: 0.81 },
   { benchmark: "GSM8K", release: "2021-11-18", solved: "2024-03-04", timeToSolve: 2.29 }
